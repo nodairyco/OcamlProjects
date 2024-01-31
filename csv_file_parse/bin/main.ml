1af3784bee851dd545eed2775eb5a784cfeeff9b
NoDairy.co <Aniashvili.Nodar@kiu.edu.ge>
2024-01-31 21:01:56 +0400

    Changed stream_manipulation.ml and main.ml and now they write to files

--- csv_file_parse/bin/main.ml
+++ csv_file_parse/bin/main.ml
@@ -1,18 +1,13 @@
 open Stream_manipulation.Stream_manipulation
 open Basic_stream_functions
-
-let temp_list = parse_list ["hi";"hello";"1";"2";"3"];;
-
-let concat_list = concat [1;2;3] [4;5;6]
+open Stream_manipulation.File_interactions
+let file_input_list = read_file "bin/test.txt";;
+let file_output_list = 
+  let a = filter file_input_list (fun x -> match x with "" -> false |_ -> true)
+in
+  let b = parse_list a 
+in 
+  map (map b (fun x -> x*x + 9)) (fun x -> string_of_int x)
 ;;
 
-(*consume concat_list (fun (x:int) -> print_endline (string_of_int x));;*)
-let print_f b = Format.printf "%b@ \n" b;;
-
-print_int (nth_element concat_list 2);
-print_endline "";
-print_int (nth_element concat_list (-2));
-print_endline "";;
-
-let example_list = map [1;2;3] (fun x -> x*x)
-;;
+write_file "bin/output.txt" file_output_list;;
--- csv_file_parse/bin/stream_manipulation.ml
+++ csv_file_parse/bin/stream_manipulation.ml
@@ -2,12 +2,19 @@
 (*this module handles manipulating streams from files in various ways*)
 
 (*making sig-type for module to hide aux methods*)
-module type Stream_manipulation = 
+module type Stream_manipulation_sig = 
 sig
   val parse_list:string list -> int list
 end;;
 
-module Stream_manipulation:Stream_manipulation = struct
+(*making sig-type for module that will read and write to files*)
+module type File_interactions_sig = sig
+  val read_file:string -> string list
+  val write_file:string -> string list -> unit
+end;;
+
+(*module that handles basic stream functions*)
+module Stream_manipulation:Stream_manipulation_sig= struct
     open Basic_stream_functions
     (*checkes if a string can be converted to number*)
     let can_be_turned_to_int input_string = 
@@ -19,11 +26,33 @@ module Stream_manipulation:Stream_manipulation = struct
     (*auxilliary method that parses a list*)
     let rec aux_parse_list (list:string list) = match list with 
     |[] -> []
-    |x::xs -> aux_parse_list xs @ [int_of_string x]
+    |x::xs -> [int_of_string x] @ aux_parse_list xs
     
     (*first filters list, than parses it*)
     let parse_list (list:string list) = 
       let filtered_list = filter list can_be_turned_to_int 
     in
       aux_parse_list filtered_list
+end;;
+
+(*module for File interactions*)
+module File_interactions:File_interactions_sig = struct
+  let read_file (file_name:string) = 
+    let file = open_in file_name in
+    let rec aux_read_file () = 
+      try
+        let line = input_line file in
+        line :: aux_read_file ()
+      with
+      |End_of_file -> []
+    in
+    aux_read_file ()
+  
+  let write_file (file_name:string) (list:string list) = 
+    let ic = open_out file_name in
+    let rec aux_write_file (list:string list) = match list with
+    |[] -> ()
+    |x::xs -> Printf.fprintf ic "%s\n" x; aux_write_file xs
+  in
+    aux_write_file list; close_out ic
 end;;
